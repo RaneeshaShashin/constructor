bba4fd250f3fba6c0018eed8c6aa6d8af69cc836
raneesha <raneeshashashin1@gmail.com>
2023-03-23 19:29:18 +0530

    constructor practicing code

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    Main(){
+        System.out.println("Java");
+        String name = "Programming Language is";
+
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+}
